51ce84f226934fd61fa75d7069e29960008e6db5
Shubham Singh <sb2001nov@gmail.com>
2022-02-24 17:50:03 +0530

    nominal data conversion
    added base documentation for all methods and added type conversion for nominal data type

--- src/base.py
+++ src/base.py
@@ -13,33 +13,77 @@ default_types = (
 
 
 class base:
+    """Base class for DataFrame and Type initialization
+    """
+
     def __init__(self, data_frame: pandas.DataFrame) -> None:
-        self.date_table: pandas.DataFrame = data_frame
+        self.__date_table: pandas.DataFrame = data_frame
         self.datatype_map = dict()
+        self.nominal_dataMap = dict()
 
         try:
-            for column in self.date_table.columns:
-                self.datatype_map[column] = str(self.date_table[column].dtype)
+            for column in self.__date_table.columns:
+                self.datatype_map[column] = str(
+                    self.__date_table[column].dtype)
         except:
-            print("exception")
             return None
 
     def set_types(self, value_dict: dict) -> bool:
+        """Set the Type for DataFrame Series.
 
+        Args:
+            value_dict (dict): dict for data types.
+
+        Returns:
+            bool: Success = 0, Failure = 1
+        """
         for i in value_dict.keys():
             if i in self.datatype_map and value_dict[i] in default_types:
+                if value_dict[i] == "nominal":
+                    unique = set(self.__date_table[i])
+                    counter = 0
+                    for unique_value in unique:
+                        self.nominal_dataMap[unique_value] = 0
+                        self.__date_table[i].replace({
+                            unique_value: counter
+                        }, inplace=True)
+                        counter += 1
                 self.datatype_map[i] = value_dict[i]
             else:
                 return 1
 
     def to_nominal(self, serise_name: str) -> bool:
+        """conver a seriese to nominal data type
+
+        Args:
+            serise_name (str): column name
+
+        Returns:
+            bool: Success = 0, Failure = 1
+        """
         if (serise_name and (serise_name in self.datatype_map.keys())):
             self.datatype_map[serise_name] = default_types[1]
+            unique = set(self.__date_table[serise_name])
+            counter = 0
+            for unique_value in unique:
+                self.nominal_dataMap[unique_value] = 0
+                self.__date_table[serise_name].replace({
+                    unique_value: counter
+                }, inplace=True)
+                counter += 1
             return 0
         else:
             return 1
 
     def to_ordinal(self, serise_name: str) -> bool:
+        """converts a series to ordinal data type
+
+        Args:
+            serise_name (str): column name
+
+        Returns:
+            bool: Success = 0, Failure = 1
+        """
         if (serise_name and (serise_name in self.datatype_map.keys())):
             self.datatype_map[serise_name] = default_types[2]
             return 0
@@ -47,6 +91,14 @@ class base:
             return 1
 
     def to_discrete(self, serise_name: str) -> bool:
+        """converts data to discrete
+
+        Args:
+            serise_name (str): column name
+
+        Returns:
+            bool: Success = 0, Failure = 1
+        """
         if (serise_name and (serise_name in self.datatype_map.keys())):
             self.datatype_map[serise_name] = default_types[3]
             return 0
@@ -54,6 +106,14 @@ class base:
             return 1
 
     def to_continuous(self, serise_name: str) -> bool:
+        """converts series to continuous
+
+        Args:
+            serise_name (str): column name
+
+        Returns:
+            bool: Success = 0, Failure = 1
+        """
         if (serise_name and (serise_name in self.datatype_map.keys())):
             self.datatype_map[series_name] = default_types[4]
             return 0
@@ -61,4 +121,4 @@ class base:
             return 1
 
     def __str__(self):
-        return f"{self.datatype_map}"
+        return f"{self.__date_table}"
